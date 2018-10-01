b5873a0fd84a20bd7b42e675ae6454e71944d5b4
Roush <roushrsh@hotmail.com>
2018-09-30 21:10:36 -0700

    Create CalcEO.py

---
+++ CalcEO.py
@@ -0,0 +1,15 @@
+#Calculating Expected Offspring. Rosalind, Soroush
+
+#http://rosalind.info/problems/iev/
+
+numberList = [18550, 17676, 19934, 17579, 18853, 17892]
+
+
+a = numberList[0] * 2
+b = numberList[1] *2
+c = numberList[2] *2
+d = numberList[3] * 0.75 *2
+e = numberList[4] * 0.5 *2
+
+sum = a + b + c + d + e
+print (sum)
